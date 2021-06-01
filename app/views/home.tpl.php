86ff051c73c13397956be0d0a1ea3ba2423c0f76
Oriane-Toque <oriane.toque@gmail.com>
2021-06-01 19:41:12 +0200

    dynamization home page, fix display number & name pokemon : ok

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -4,6 +4,7 @@
   <?php foreach($viewVars['pokemons'] as $pokemon) : ?>
     <div class="pokemonList_pokemon">
       <img src="img/<?= $pokemon->getNumero(); ?>.png" alt="pokemon">
+      <p><span>#<?= $pokemon->getNumero(); ?></span> <?= $pokemon->getNom(); ?></p>
     </div>
   <?php endforeach; ?>
   </section>
