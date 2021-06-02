5988178dccd329b849b05a83c5c653a2c9a97c09
Oriane-Toque <oriane.toque@gmail.com>
2021-06-02 15:41:15 +0200

    wip mobile first css - home & details page : done

--- app/views/details.tpl.php
+++ app/views/details.tpl.php
@@ -1,43 +1,62 @@
 <section class="pokemon">
-  <h1>Détails de <?= $viewVars['pokemon']->getNom(); ?></h1>
-  <div class="description">
+  <h2>Détails de <?= $viewVars['pokemon']->getNom(); ?></h2>
+  <div class="pokemonDescription">
     <aside>
       <img src="<?= $_SERVER['BASE_URI']; ?>/img/<?= $viewVars['pokemon']->getNumero(); ?>.png" alt="<?= $viewVars['pokemon']->getNom(); ?>">
     </aside>
     <article>
-      <h2><?= $viewVars['pokemon']->getNom(); ?></h2>
+      <h3><?= '#'.$viewVars['pokemon']->getNumero().' '.$viewVars['pokemon']->getNom(); ?></h3>
       <?php foreach($viewVars['types'] as $type) : ?>
-      <div class="type">
+      <div class="pokemonType">
         <p><span style="background-color: #<?= $type->getColor(); ?>;"><?= $type->getName(); ?></span></p>
       </div>
       <?php endforeach; ?>
-      <div class="stats">
+      <div class="pokemonStats">
+
         <div>
           <label for="pv">PV<span><?= $viewVars['pokemon']->getPv(); ?></span></label>
-          <progress id="pv" value="<?= $viewVars['pokemon']->getPv(); ?>" max="255"><?= $viewVars['pokemon']->getPv(); ?></progress>
+          <div class="progress">
+            <div role="progressbar" style="width: <?= ($viewVars['pokemon']->getPv()*100)/255; ?>%;"></div>
+          </div>
         </div>
+
         <div>
           <label for="attack">Attaque<span><?= $viewVars['pokemon']->getAttaque(); ?></span></label>
-          <progress id="attack" value="<?= $viewVars['pokemon']->getAttaque(); ?>" max="255"><?= $viewVars['pokemon']->getAttaque(); ?></progress>
+          <div class="progress">
+            <div role="progressbar" style="width: <?= ($viewVars['pokemon']->getAttaque()*100)/255; ?>%;"></div>
+          </div>
         </div>
+
         <div>
           <label for="defense">Défense<span><?= $viewVars['pokemon']->getDefense(); ?></span></label>
-          <progress id="defense" value="<?= $viewVars['pokemon']->getDefense(); ?>" max="255"><?= $viewVars['pokemon']->getDefense(); ?></progress>
+          <div class="progress">
+            <div role="progressbar" style="width: <?= ($viewVars['pokemon']->getDefense()*100)/255; ?>%;"></div>
+          </div>
         </div>
+
         <div>
-          <label for="attackSpe">attaque Spé.<span><?= $viewVars['pokemon']->getAttaqueSpe(); ?></span></label>
-          <progress id="attackSpe" value="<?= $viewVars['pokemon']->getAttaqueSpe(); ?>" max="255"><?= $viewVars['pokemon']->getAttaqueSpe(); ?></progress>
+          <label for="attackSpe">Attaque Spé.<span><?= $viewVars['pokemon']->getAttaqueSpe(); ?></span></label>
+          <div class="progress">
+            <div role="progressbar" style="width: <?= ($viewVars['pokemon']->getAttaqueSpe()*100)/255; ?>%;"></div>
+          </div>
         </div>
+
         <div>
           <label for="defenseSpe">Défense Spé.<span><?= $viewVars['pokemon']->getDefenseSpe(); ?></span></label>
-          <progress id=="defenseSpe" value="<?= $viewVars['pokemon']->getDefenseSpe(); ?>" max="255"><?= $viewVars['pokemon']->getDefenseSpe(); ?></progress>
+          <div class="progress">
+            <div role="progressbar" style="width: <?= ($viewVars['pokemon']->getDefenseSpe()*100)/255; ?>%;"></div>
+          </div>
         </div>
+
         <div>
           <label for="speed">Vitesse<span><?= $viewVars['pokemon']->getVitesse(); ?></span></label>
-          <progress id="speed" value="<?= $viewVars['pokemon']->getVitesse(); ?>" max="255"><?= $viewVars['pokemon']->getVitesse(); ?></progress>
+          <div class="progress">
+            <div role="progressbar" style="width: <?= ($viewVars['pokemon']->getVitesse()*100)/255; ?>%;"></div>
+          </div>
         </div>
+
       </div>
     </article>
   </div>
-  <a href="<?= $router->generate('main.home'); ?>">Revenir à la liste</a>
+  <p class="pokemonBackHome" role="back to home page"><a href="<?= $router->generate('main.home'); ?>">Revenir à la liste</a></p>
 </section>
