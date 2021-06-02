919d5ddb8052ebebf4e6d9a3ac14847ce5991f58
Oriane-Toque <oriane.toque@gmail.com>
2021-06-02 16:28:16 +0200

    css mobile first : done

--- app/views/list.types.tpl.php
+++ app/views/list.types.tpl.php
@@ -1,8 +1,8 @@
-<section class="types">
-  <p>Cliquez sur un type</p>
+<section class="listTypes">
+  <h2>Cliquez sur un type pour voir tous les Pokemons de ce type</h2>
   <?php foreach($viewVars['types'] as $type) : ?>
-  <div>
-    <p style="background-color: #<?= $type->getColor(); ?>;"><a href="<?= $router->generate('catalog.pokemon', ['id' => $type->getId()]); ?>"><?= $type->getName(); ?></a></p>
-  </div>
+    <div>
+      <p style="background-color: #<?= $type->getColor(); ?>;"><a href="<?= $router->generate('catalog.pokemon', ['id' => $type->getId()]); ?>"><?= $type->getName(); ?></a></p>
+    </div>
   <?php endforeach; ?>
 </section>
