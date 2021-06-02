b3aae6a0500624316ac24ed827b6bb10cd9b965c
Oriane-Toque <oriane.toque@gmail.com>
2021-06-02 17:26:26 +0200

    responsive design : done

--- app/views/list.types.tpl.php
+++ app/views/list.types.tpl.php
@@ -2,7 +2,9 @@
   <h2>Cliquez sur un type pour voir tous les Pokemons de ce type</h2>
   <?php foreach($viewVars['types'] as $type) : ?>
     <div>
-      <p style="background-color: #<?= $type->getColor(); ?>;"><a href="<?= $router->generate('catalog.pokemon', ['id' => $type->getId()]); ?>"><?= $type->getName(); ?></a></p>
+      <a href="<?= $router->generate('catalog.pokemon', ['id' => $type->getId()]); ?>">
+        <p style="background-color: #<?= $type->getColor(); ?>;"><?= $type->getName(); ?></p>
+      </a>
     </div>
   <?php endforeach; ?>
 </section>
